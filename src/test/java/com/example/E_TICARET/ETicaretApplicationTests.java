0f0bf8fd208dfd8a5b9f4339b594a31de4e1500b
Reus99 <berkoncu99@gmail.com>
2025-09-04 19:52:26 +0300

    Springboot projesi oluşturuldu

---
+++ src/test/java/com/example/E_TICARET/ETicaretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_TICARET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicaretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
